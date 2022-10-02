acedd0611e9bcbd425c569d6a1c2abb0ef4449c3
Haroon Atif <71429280+Haroon-Atif@users.noreply.github.com>
2022-10-02 10:47:45 -0700

    Add files via upload

--- NASA_SpaceAppsChallenge.py
+++ NASA_SpaceAppsChallenge.py
@@ -11,11 +11,18 @@ import re
 from wordcloud import WordCloud
 import nltk
 import heapq
+import PIL.Image
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
 
 
 #working path for the project 
 path = os.path.expanduser('D:\\') + 'Projects/NASA SpaceAppsChallenge/'
 id_folder_path = path + "id_data"
+bar_path = path + "bar_charts"
+wordcl_path = path + "wordcloud_charts"
+report_path = path + "reports"
 
 
 #Make RESTful Api requests and get the data
@@ -95,15 +102,12 @@ def process_data(items):
    # return processed_data
 
 
-#Pass the data to NLTK 
-#should make a list of topic key words found in each document,
-#including the frequency with which those key words appear. 
-#should summarize the document
-def analyze_data(processed_data):
-    pass
-   # return summary, key_words
-
 def chart_data(word_data):
+    #Create folders for graphs if not exists
+    if not os.path.exists(bar_path):
+        os.makedirs(bar_path)
+    if not os.path.exists(wordcl_path):
+        os.makedirs(wordcl_path)
     #Data should be in the format {"word1": 23, word2: 234, word3: 32, word4: 76, word5: 98, word6: 38, word7: 65, word8: 83, word9: 75, word10: 45, word11: 12, word12: 12}
     # test_data = {'word1': 23, 'word2': 234, 'word3': 32, 'word4': 76, 'word5': 98, 'word6': 38, 'word7': 65, 'word8': 83, 'word9': 75, 'word10': 45, 'word11': 12, 'word12': 12}
     
@@ -112,39 +116,57 @@ def chart_data(word_data):
     for i, v in enumerate(word_data):
         mod_data = {'Word': v.keys(), 'Count': v.values()}
         df = pd.DataFrame(data=mod_data)
-    
-        g1 = sns.catplot(y="Count", x='Word', kind="bar", data=df)
-        fig1 = g1.fig
-        fig1.savefig('barplot{}.png'.format(i))
+        
 
+        g = sns.catplot(y="Count", x='Word', kind="bar", data=df)
+        g.set_xticklabels(fontsize=8,rotation=75)
+        g.fig.set_size_inches(16,9)
+        fig1 = g.fig
+        fig1.savefig('bar_charts/barplot{}.png'.format(i))
 
         wordcloud = WordCloud()
         wordcloud.generate_from_frequencies(frequencies=v)
         plt.figure()
         plt.imshow(wordcloud, interpolation="bilinear")
         plt.axis("off")
-        plt.savefig("wordcloud{}.png".format(i))
-        plt.show()
+        plt.savefig("wordcloud_charts/wordcloud{}.png".format(i))
+        plt.close()
+        
+
+def image_process():
+    for image in os.listdir(bar_path):
+        if '.' not in image[0]:
+            with PIL.Image.open(bar_path+'/'+image) as im:
+                im.resize((300,300)).convert("RGB").save(bar_path+'/'+ image.split(".")[0] + ".jpeg", 'JPEG')
+    for image in os.listdir(wordcl_path):
+        if '.' not in image[0]:
+            with PIL.Image.open(wordcl_path+'/'+image) as im:
+                im.resize((300,300)).convert("RGB").save(wordcl_path+'/'+ image.split(".")[0] + ".jpeg", 'JPEG')
+
 
 #Create PDF report of file keywords and summaries
 #and the titles of the docs analyzed 
 #include download links to the file 
-def generate_report(filename, title, summaries, key_words):
-    
-#basic reports generator function
-  #  styles = getSampleStyleSheet()
-  #  report = SimpleDocTemplate(filename)
-  #  report_title = Paragraph(title, styles["h1"])
-  #  report_info = Paragraph(summaries, styles["BodyText"])
-  #  empty_line = Spacer(1,20)
-  #  report.build([report_title, empty_line, report_info])
-    pass
-   # return report
+def generate_report(filename, title, summaries):
+  if not os.path.exists(report_path):
+        os.makedirs(report_path)  
+  for i in range(24):
+      styles = getSampleStyleSheet()
+      report = SimpleDocTemplate('reports/{}{}.pdf'.format(filename,i))
+      report_title = Paragraph('{} {}'.format(title,i), styles["h1"])
+      report_info = Paragraph(summaries[i], styles["BodyText"])
+      empty_line = Spacer(1,20)
+      report_image1 = Image('bar_charts/barplot{}.jpeg'.format(i))
+      report_image2 = Image("wordcloud_charts/wordcloud{}.jpeg".format(i))
+      report.build([report_title, empty_line, report_info,report_image1,report_image2])
+   
+  
 
 
 if __name__ ==  "__main__":
     data = get_data()
     summary, word_data = process_data(data)
-    chart_data(word_data)
-
+    #chart_data(word_data)
+    image_process()
+    generate_report('report', 'Analyzed Report', summary)
     
